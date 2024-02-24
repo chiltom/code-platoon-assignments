3540278e5d6ee5e0627ed7049b5d1eadc38e1db6
Tom Childress <thomas.childress02@gmail.com>
2024-02-23 22:05:01 -0500

    added API GET endpoint to return subjects and their teacher and students

--- flask_postgres_school/app.py
+++ flask_postgres_school/app.py
@@ -78,5 +78,25 @@ def get_teachers() -> list:
         teacher_list.append({'first_name': teacher.first_name, 'last_name': teacher.last_name, 'age': teacher.age, 'subject': subject_dict})
     return jsonify(teacher_list)
 
+# Returns a list of dictionaries with the students enrolled in each class and the teacher who teaches each subject
+@app.route('/subjects/', methods=['GET'])
+def get_subjects() -> list:
+    students = Students.query.all()
+    subjects = Subjects.query.all()
+    teachers = Teachers.query.all()
+    subject_list = []
+    for subject in subjects:
+        subject_dict = {'id': subject.id, 'subject': subject.subject}
+        student_list = []
+        for teacher in teachers:
+            if subject.id == teacher.subject:
+                subject_dict['teacher'] = f"{teacher.first_name} {teacher.last_name}"
+        for student in students:
+            if subject.id == student.subject:
+                student_list.append(f"{student.first_name} {student.last_name}")
+        subject_dict['students'] = student_list
+        subject_list.append(subject_dict)
+    return jsonify(subject_list)
+
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
