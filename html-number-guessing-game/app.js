33b3f9dbf32ec6a986bb8e629c701f9d6c31793c
Tom Childress <thomas.childress02@gmail.com>
2024-02-26 15:42:33 -0500

    completed functionality for guessing game with event listener, next step is styling the game to be more aesthetically pleasing and refactoring scripts

--- html-number-guessing-game/app.js
+++ html-number-guessing-game/app.js
@@ -4,6 +4,10 @@ function getNum() {return Math.floor(Math.random() * 100) + 1};
 const randNum = getNum();
 console.log(randNum);
 
+// Grab elements for input data and guess history from document
+const input = document.getElementById("userGuess");
+const output = document.getElementById("guessHistory");
+
 // Function to compare guess to random number
 const compareNums = (num) => {
     if (num === randNum) {
@@ -19,20 +23,30 @@ const compareNums = (num) => {
 let guessNum = 0;
 
 // Add event listener to form to grab input and run compare nums function
-const input = document.getElementById("userGuess");
 console.log(input);
 input.addEventListener("submit", (e) => {
     // Prevent default action from happening with submission (refresh)
     e.preventDefault();
+
     // Increment guess counter
     guessNum += 1;
+
     // Grab data from form with FormData and create an object from it
     const data = new FormData(e.target);
     const formattedData = Object.fromEntries(data);
+
     // Add ID to formattedData
     formattedData['id'] = guessNum;
-    // TODO: Make elements to hold guess data
-    console.log(formattedData)
-    
 
+    // Create element to hold guess data and append it to output list
+    const li = document.createElement('li');
+    li.id = formattedData.id;
+    li.textContent = compareNums(Number(formattedData.guess));
+    output.appendChild(li);
+    
+    // Check if input matches randNum and, if so, remove it
+    if (li.textContent === compareNums(randNum)) {
+        const container = document.getElementById("gameContainer");
+        container.removeChild(input);
+    }
 });
