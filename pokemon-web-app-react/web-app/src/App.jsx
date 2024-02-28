44b9cbb69936501752a40df1369f1f145e530e97
Tom Childress <thomas.childress02@gmail.com>
2024-02-28 15:56:00 -0500

    created basic structure of app with minor css and jsx structure

--- pokemon-web-app-react/web-app/src/App.jsx
+++ pokemon-web-app-react/web-app/src/App.jsx
@@ -2,30 +2,23 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.parentElement);
+  }
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='headerCont'>
+        <h1 className='headerText'>PokeAPI Fetcher<img src='https://showme0-9071.kxcdn.com/files/528810/pictures/thumbs/1189951/last_thumb1382570134.jpg' width='100px' height='100px' id='happyPikachu'></img></h1>
+        <p className='headerText'><em>A simple Pokemon data fetcher</em></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='inputCont'>
+        <form id='userInput' onSubmit={handleSubmit}>
+          <input type='text' id='textInput' placeholder="Enter a pokemon's name:"></input>
+          <input type='submit'></input>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-
     </>
   )
 }
