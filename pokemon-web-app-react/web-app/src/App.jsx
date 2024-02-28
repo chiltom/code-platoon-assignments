d80e25d066b86bd2878744defd8985110a0166e3
Tom Childress <thomas.childress02@gmail.com>
2024-02-28 17:56:22 -0500

    added functionality to grab data from API - need to use useEffects to refresh the hooks faster

--- pokemon-web-app-react/web-app/src/App.jsx
+++ pokemon-web-app-react/web-app/src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [pokeName, setPokeName] = useState("");
+  const [pokeData, setPokeData] = useState(null);
+  const handlePokeNameChange = e => setPokeName(e.target.value);
+  // const handlePokeDataChange = e => setPokeData(e.target.value);
+
+  const getPokemon = async (target) => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${target}`);
+      setPokeData(response.data);
+    } catch(error) {
+      setPokeData(`${error.response.status}: ${error.response.data}`);
+    }
+  }
+
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(e.target.parentElement);
+    
+    getPokemon(pokeName);
+    console.log(pokeData);
+    
+    const outputCont = document.getElementById('outputCont');
+    const card = document.createElement('div');
+    card.className = 'card';
+    outputCont.appendChild(card);
+
+    const cardData = document.createElement('p');
+    cardData.className = 'cardData'
+    cardData.textContent = pokeData;
+    card.appendChild(cardData);
   }
 
   return (
@@ -15,9 +42,14 @@ function App() {
       </div>
       <div id='inputCont'>
         <form id='userInput' onSubmit={handleSubmit}>
-          <input type='text' id='textInput' placeholder="Enter a pokemon's name:"></input>
+          <input type='text' name='pokeName' placeholder="Enter a pokemon's name:" value={pokeName} onChange={handlePokeNameChange}></input>
           <input type='submit'></input>
         </form>
+      </div>
+      <div id='outputCont'>
+        <div className='card'>
+          <p>Charizard</p>
+        </div>
       </div>
     </>
   )
