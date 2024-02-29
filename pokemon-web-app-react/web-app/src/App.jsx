fe0996502cce0d72f209c9d035c5e460c1ad5b96
Tom Childress <thomas.childress02@gmail.com>
2024-02-28 21:03:46 -0500

    used async function in event handler to ensure response data was waited for, card addition functionality completed

--- pokemon-web-app-react/web-app/src/App.jsx
+++ pokemon-web-app-react/web-app/src/App.jsx
@@ -3,35 +3,65 @@ import axios from 'axios';
 import './App.css'
 
 function App() {
+  // State hooks and handlers for hooks
   const [pokeName, setPokeName] = useState("");
-  const [pokeData, setPokeData] = useState(null);
   const handlePokeNameChange = e => setPokeName(e.target.value);
-  // const handlePokeDataChange = e => setPokeData(e.target.value);
 
-  const getPokemon = async (target) => {
-    try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${target}`);
-      setPokeData(response.data);
-    } catch(error) {
-      setPokeData(`${error.response.status}: ${error.response.data}`);
+  // Helper Functions
+  // Change a word to title case
+  const toTitleCase = (str) => {
+    const arr = str.split("");
+    arr[0] = arr[0].toUpperCase();
+    return arr.join("");
+  }
+  // Create a string of pokemon types based on their returned list of types
+  const typeString = (types) => {
+    let output = "Types: ";
+    for (let type of types) {
+      console.log(type);
+      output += `${toTitleCase(type.type.name)} `;
     }
+    return output
   }
 
-  const handleSubmit = e => {
+  // Handle form submission for pokemon search, make asynchronous to handle response time from api request
+  const handleSubmit = async (e) => {
+    // Prevent page refresh
     e.preventDefault();
-    
-    getPokemon(pokeName);
-    console.log(pokeData);
-    
+    // Grab data from pokeapi with pokeName that was handled onChange when the name was input (line 72)
+    let data = null;
+    // Use try catch to catch bad responses and post error, else grab data return from pokeapi
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}`);
+      data = await response.data;
+    } catch (error) {
+      data = `${error.response.status}: ${error.response.data}`;
+    }
+    // Debugging console.log
+    console.log(data);
+
+    // Grab output container to hold pokemon "cards"
     const outputCont = document.getElementById('outputCont');
+    // Create pokemon "card" to hold data
     const card = document.createElement('div');
     card.className = 'card';
     outputCont.appendChild(card);
-
-    const cardData = document.createElement('p');
-    cardData.className = 'cardData'
-    cardData.textContent = pokeData;
-    card.appendChild(cardData);
+    // Create header for card name and append to card
+    const cardName = document.createElement('h3');
+    cardName.className = 'cardName'
+    cardName.textContent = toTitleCase(data.name);
+    card.appendChild(cardName);
+    // Add pokemon types to card
+    const cardTypes = document.createElement('p');
+    cardTypes.className = 'cardTypes';
+    console.log(data.types);
+    cardTypes.textContent = typeString(data.types);
+    card.appendChild(cardTypes);
+    // Add pokemon picture to card
+    const cardImg = document.createElement('img');
+    cardImg.className = 'cardImg'
+    cardImg.src = data.sprites.front_default;
+    card.appendChild(cardImg);
   }
 
   return (
@@ -48,7 +78,6 @@ function App() {
       </div>
       <div id='outputCont'>
         <div className='card'>
-          <p>Charizard</p>
         </div>
       </div>
     </>
