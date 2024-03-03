25e6cd51c7106e8cc33dbca16864eb71ffc64c71
Tom Childress <thomas.childress02@gmail.com>
2024-03-03 07:44:59 -0500

    updated hangman assignment

--- react-hangman/src/App.jsx
+++ react-hangman/src/App.jsx
@@ -1,39 +1,11 @@
-import { useState, useEffect } from 'react'
-import words from './assets/words.jsx'
+import words from './assets/words.json'
 import './App.css'
-import Game from './Game.jsx'
 
 function App() {
-  const [puzzle, setPuzzle] = useState([]);
-  const [guessedLetters, setGuessedLetters] = useState([]);
-  const [input, setInput] = useState("");
-
-  useEffect(() => {
-    setPuzzle((words[Math.floor(Math.random() * words.length)]).split(""));
-  }, []);
-
-  const handleInput = e => setInput(e.target.value);
-
-  const handleSubmission = e => {
-    e.preventDefault();
-    setGuessedLetters([...guessedLetters, input]);
-    setInput("");
-  };
 
   return (
     <>
-      <h1>Hangman</h1>
-      <div id='puzzleCont'>
-        <Game arr={puzzle}/>
-        <p id='guessedLetters'>{guessedLetters}</p>
-      </div>
-      <div id='inputCont'>
-        <form onSubmit={handleSubmission}>
-          <input type='text' name='guess' value={input} onChange={handleInput}/>
-          <input type='submit'/>
-        </form>
-      </div>
-
+      
     </>
   )
 }
