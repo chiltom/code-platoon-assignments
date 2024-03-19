8bbb760dc633708deaa37c4009bb55a878cf15d5
Tom Childress <thomas.childress02@gmail.com>
2024-03-19 16:48:04 -0400

    altered class methods for student model

--- django-school-api-II/school_proj/student_app/models.py
+++ django-school-api-II/school_proj/student_app/models.py
@@ -4,20 +4,22 @@ from django.db import models
 
 
 class Student(models.Model):
-    name = models.CharField(max_length=255, null=False, blank=False)
-    student_email = models.EmailField(null=False, blank=False, unique=True)
+    name = models.CharField(max_length=255)
+    student_email = models.EmailField(unique=True)
     personal_email = models.EmailField(null=True, blank=True, unique=True)
     locker_number = models.IntegerField(
-        default=110, null=False, blank=False, unique=True)
+        default=110, unique=True)
     locker_combination = models.CharField(
-        max_length=255, null=False, blank=False, default="12-12-12")
+        max_length=255, default="12-12-12")
     good_student = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f"{self.name} - {self.student_email} - {self.locker_number}"
 
-    def locker_reassignment(self, num) -> None:
+    def locker_reassignment(self, num: int) -> None:
         self.locker_number = num
+        self.save()
 
-    def student_status(self, new) -> bool:
+    def student_status(self, new: bool) -> bool:
         self.good_student = new
+        self.save()
