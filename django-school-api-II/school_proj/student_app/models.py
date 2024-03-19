f30333dfa73c8aa67092cac3c329c71a7433e6de
Tom Childress <thomas.childress02@gmail.com>
2024-03-19 14:49:01 -0400

    finished django-school-api-II assignment

--- django-school-api-II/school_proj/student_app/models.py
+++ django-school-api-II/school_proj/student_app/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Student(models.Model):
-    name = models.CharField(max_length=255, null = False, blank = False)
-    student_email = models.EmailField(null = False, blank = False)
-    personal_email = models.EmailField(null = False, blank = False)
-    locker_number = models.IntegerField(default=1, null = False, blank = False)
-    locker_combination = models.CharField(max_length=255, null = False, blank = False)
-    good_student = models.BooleanField()
+    name = models.CharField(max_length=255, null=False, blank=False)
+    student_email = models.EmailField(null=False, blank=False, unique=True)
+    personal_email = models.EmailField(null=True, blank=True, unique=True)
+    locker_number = models.IntegerField(
+        default=110, null=False, blank=False, unique=True)
+    locker_combination = models.CharField(
+        max_length=255, null=False, blank=False, default="12-12-12")
+    good_student = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+
+    def locker_reassignment(self, num) -> None:
+        self.locker_number = num
+
+    def student_status(self, new) -> bool:
+        self.good_student = new
