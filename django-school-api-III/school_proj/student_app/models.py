797bc282e2138b693b0f25207c30ca391ecb33f0
Tom Childress <thomas.childress02@gmail.com>
2024-03-20 16:18:24 -0400

    finished django-school-api-III assignment

---
+++ django-school-api-III/school_proj/student_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core import validators as v
+from .validators import *
+
+# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255, null=False,
+                            blank=False, validators=[validate_name_format])
+    student_email = models.EmailField(
+        null=False, blank=False, unique=True, validators=[validate_school_email])
+    personal_email = models.EmailField(null=True, blank=True, unique=True)
+    locker_number = models.IntegerField(
+        default=110, null=False, blank=False, unique=True, validators=[v.MinValueValidator(1), v.MaxValueValidator(200)])
+    locker_combination = models.CharField(
+        default="12-12-12", null=False, blank=False, max_length=255, validators=[validate_combination_format])
+    good_student = models.BooleanField(default=True)
