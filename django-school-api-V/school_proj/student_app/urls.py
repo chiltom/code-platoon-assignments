5b935cc093bcdddf35678e58908dee5416a63e52
Tom Childress <thomas.childress02@gmail.com>
2024-03-22 15:24:19 -0400

    cleaned up progressive assignments

--- django-school-api-V/school_proj/student_app/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import All_students
-from django.urls import path
-
-urlpatterns = [
-    path('', All_students.as_view(), name="all_students")
-]
--- django-school-api-V/school_proj/subject_app/urls.py
+++
@@ -1,6 +0,0 @@
-from .views import All_subjects
-from django.urls import path
-
-urlpatterns = [
-    path('', All_subjects.as_view(), name="all_subjects")
-]
