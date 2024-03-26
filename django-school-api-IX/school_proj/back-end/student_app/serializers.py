6461153bcf533c063ecb0445a5d8e3d26f02c49c
Tom Childress <thomas.childress02@gmail.com>
2024-03-26 11:09:23 -0400

    cloned school-api-IX assignment from curriculum repo

---
+++ django-school-api-IX/school_proj/back-end/student_app/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Student
+from subject_app.serializers import SubjectSerializer
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ["name", "student_email", "locker_number"]
+
+
+class StudentAllSerializer(serializers.ModelSerializer):
+    subjects = SubjectSerializer(many=True)
+
+    class Meta:
+        model = Student
+        fields = [
+            "id",
+            "name",
+            "student_email",
+            "personal_email",
+            "locker_number",
+            "locker_combination",
+            "good_student",
+            "subjects",
+        ]
+
+        # def get_subjects(self, obj):
+        #     return SubjectSerializer(obj.students.all(), many=True).data
