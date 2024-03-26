06e35ad9c23a53b51a765e4f74a8362337f5988a
Tom Childress <thomas.childress02@gmail.com>
2024-03-26 14:44:39 -0400

    created a route that will allow you to add subjects to students

--- django-school-api-cumulative/student_app/views.py
+++ django-school-api-cumulative/student_app/views.py
@@ -1,4 +1,5 @@
 from .models import Student
+from subject_app.models import Subject
 from .serializers import StudentAllSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,9 +16,13 @@ class All_students(APIView):
 
 
 class A_student(APIView):
-    # def put_subjects(self, lst_of_subjects):
-    #     for subj in lst_of_subjects:
-    #         if subj in
+    # TODO: See if we can add AND with a statement to check if student
+    # already has class (and not student.subjects.get(subj_id))
+    def update_subjects(self, student, lst_of_subject_ids):
+        for subj_id in lst_of_subject_ids:
+            if get_object_or_404(Subject, id=subj_id):
+                student.subjects.add(subj_id)
+                student.save()
 
     def get(self, request, id):
         student = StudentAllSerializer(get_object_or_404(Student, id=id))
@@ -29,6 +34,9 @@ class A_student(APIView):
             student, data=request.data, partial=True)
         if ser_student.is_valid():
             ser_student.save()
+            if request.data.get("lst_of_subjects"):
+                self.update_subjects(
+                    student, request.data.get("lst_of_subjects"))
             return Response(ser_student.data, status=HTTP_200_OK)
         else:
             print(ser_student.errors)
