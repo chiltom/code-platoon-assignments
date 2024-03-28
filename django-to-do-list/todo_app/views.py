abd71bc256d2fc9650e1fe603eeab59f4e7d7749
Tom Childress <thomas.childress02@gmail.com>
2024-03-27 20:54:06 -0400

    created put method in a_task view

--- django-to-do-list/todo_app/views.py
+++ django-to-do-list/todo_app/views.py
@@ -78,6 +78,24 @@ class All_tasks(APIView):
 
 
 class A_task(APIView):
+    def add_sub_tasks(self, task, lst_of_sub_task_ids):
+        for sub_task_id in lst_of_sub_task_ids:
+            if get_object_or_404(Sub_Task, id=sub_task_id):
+                task.sub_tasks.add(sub_task_id)
+                task.save()
+
     def get(self, request, id, task_id):
         task = TaskSerializer(get_object_or_404(Task, id=task_id))
         return Response(task.data, status=HTTP_200_OK)
+
+    def put(self, request, id, task_id):
+        data = request.data.copy()
+        task = get_object_or_404(Task, id=task_id)
+        ser_task = TaskSerializer(task, data=data, partial=True)
+        if ser_task.is_valid():
+            ser_task.save()
+            if data.get("lst_of_sub_tasks"):
+                self.add_sub_tasks(task, data.get("lst_of_sub_tasks"))
+            return Response(ser_task.data, status=HTTP_200_OK)
+        else:
+            return Response(ser_task.errors, status=HTTP_400_BAD_REQUEST)
